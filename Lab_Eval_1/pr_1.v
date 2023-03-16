766a4dd34809ce5775f1de06eee16467f1d45b5c
Ashwin Narayanan S <ashrockzzz2003@gmail.com>
2023-03-16 09:41:23 +0530

    optimized

--- Lab_Eval_1/pr_1.v
+++ Lab_Eval_1/pr_1.v
@@ -2,14 +2,22 @@ module pr_1();
     reg B3, B2, B1, B0;
     wire D3, D2, D1, D0, w1, w2, w3, w4, w5, w6, not_b0, not_b1, not_b2, not_b3;
 
+    // D3
+    assign D3 = B3;
+
+    // D2, D1, D0, Approach 2, Optimized
+    xor(D2, B3, B2);
+    xor(D1, B2, B1);
+    xor(D0, B1, B0);
+
+/* Approach 1, Naive
+
     not(not_b3, B3);
     not(not_b2, B2);
     not(not_b1, B1);
     not(not_b0, B0);
-
     // D3
     assign D3 = B3;
-
     // D2
     and(w1, not_b3, B2);
     and(w2, B3, not_b2);
@@ -24,6 +32,8 @@ module pr_1();
     and(w5, not_b1, B0);
     and(w6, B1, not_b0);
     or(D0, w5, w6);
+    
+*/
 
     // Test Bench
     initial begin
